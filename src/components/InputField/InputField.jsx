ac35536ccc6b99b55e3aaad8eabce6123ac155fd
Alex <alexanderbigar@gmail.com>
2022-06-10 01:09:20 +0300

    update inputs section

---
+++ src/components/InputField/InputField.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from "classnames";
+
+import styles from './style.module.css';
+
+const InputField = ({ inputWrapperStyle, backgroundPositionY, iconHeight, iconWidth,  inputHeight, inputType, icon, inputPlaceholder }) => {
+    const inputStyle = {
+        height: inputHeight,
+        background: `url(${icon}) no-repeat scroll ${iconWidth} ${iconHeight}`,
+        backgroundPosition: '22px center',
+        backgroundPositionY: backgroundPositionY,
+    }
+
+    return (
+        <div className={classNames(styles.inputWrapper, inputWrapperStyle)}>
+            <span className={styles.inputLine} />
+            <input className={styles.input} type={inputType} placeholder={inputPlaceholder} style={inputStyle} />
+        </div>
+
+    );
+};
+
+export default InputField;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import LinkButton from "../../components/LinkButton/LinkButton";
+import Headers from "../../components/Headers/Headers";
+import Card from "../../components/Card/Card";
+import LandingSlider from "../../components/LandingSlider/LandingSlider";
+import AboutSection from "../../components/AboutSection/AboutSection";
+import ContentContainer from "../../components/ContentContainer/ContentContainer";
+import InputField from "../../components/InputField/InputField";
 import backgroundImg from '../../assets/images/background.jpg';
 import webDesignBg from '../../assets/images/webdesign-bg.jpg';
 import speaker from '../../assets/icons/speaker.svg';
 import gear from '../../assets/icons/gear.svg';
 import docs from '../../assets/icons/docs.svg';
 import lock from '../../assets/icons/lock.svg';
-import LinkButton from "../../components/LinkButton/LinkButton";
-import Headers from "../../components/Headers/Headers";
-import Card from "../../components/Card/Card";
-import LandingSlider from "../../components/LandingSlider/LandingSlider";
-import AboutSection from "../../components/AboutSection/AboutSection";
+import personIcon from '../../assets/icons/person.svg';
+import emailIcon from '../../assets/icons/email.svg';
+import penIcon from '../../assets/icons/pen.svg';
 
 import styles from './style.module.css';
+
 
 const sectionStyle = {
     height: "100vh",
@@ -32,24 +38,6 @@ const HomePage = () => {
                 <Headers headerStyle={styles.homePageHeader} children={'Torem'} />
                 <LinkButton linkButtonStyle={styles.homePageLinkButton} children={'Contact us'}/>
             </section>
-            {/*<section className={styles.homePageAboutUs}>*/}
-            {/*    <img className={styles.homePageAboutUsImg} src={pin} alt={'Pin'}/>*/}
-            {/*    <ContentContainer>*/}
-            {/*        <Headers headerStyle={styles.homePageAboutUsHeader} children={'About us'} />*/}
-            {/*        <div className={styles.homePageAboutUsTextWrapper}>*/}
-            {/*            <div className={styles.homePageAboutUsText}>*/}
-            {/*                It is a long established fact that a reader will be distracted by the readable content of a*/}
-            {/*                page when looking at its layout. The point of using Lorem Ipsum is that it has a*/}
-            {/*                more-or-less normal distribution of letters*/}
-            {/*            </div>*/}
-            {/*            <div className={styles.homePageAboutUsText}>*/}
-            {/*                It is a long established fact that a reader will be distracted by the readable content of a*/}
-            {/*                page when looking at its layout. The point of using Lorem Ipsum is that it has a*/}
-            {/*                more-or-less normal distribution of letters*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </ContentContainer>*/}
-            {/*</section>*/}
             <AboutSection
                 sectionTitle={'About us'}
                 columnOneText={'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters'}
@@ -98,6 +86,42 @@ const HomePage = () => {
                 columnOneText={'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters'}
                 columnTwoText={'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters'}
             />
+            <section className={styles.homePageInputSectionWrapper}>
+                <ContentContainer>
+                    <div className={styles.inputWrapper}>
+                        <Headers headerStyle={styles.inputHeader} children={'Get in Touch'} />
+                        <span className={styles.inputLine} />
+                        <div className={styles.inputDescription}>Feel free to drop us a form belong</div>
+                        <InputField
+                            inputWrapperStyle={styles.inputsWrapperClass}
+                            icon={personIcon}
+                            iconWidth={'16px'}
+                            iconHeight={'16px'}
+                            backgroundPositionY={'19px'}
+                            inputType={'text'}
+                            inputPlaceholder={'Name'}
+                        />
+                        <InputField
+                            inputWrapperStyle={styles.inputsWrapperClass}
+                            icon={emailIcon}
+                            iconWidth={'17px'}
+                            iconHeight={'12px'}
+                            backgroundPositionY={'21px'}
+                            inputType={'email'}
+                            inputPlaceholder={'Your email'}
+                        />
+                        <InputField
+                            inputWrapperStyle={styles.inputsWrapperClass}
+                            icon={penIcon}
+                            iconWidth={'20px'}
+                            iconHeight={'20px'}
+                            backgroundPositionY={'17px'}
+                            inputType={'text'}
+                            inputPlaceholder={'Ask your question'}
+                        />
+                    </div>
+                </ContentContainer>
+            </section>
         </>
     );
 };
